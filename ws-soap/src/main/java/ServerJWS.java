38f1040fb0eb87338295123b11e4d7e046a7f5ed
Ayoub <ayoub@I.local>
2024-07-09 10:36:44 +0100

    Tp5

---
+++ ws-soap/src/main/java/ServerJWS.java
@@ -0,0 +1,13 @@
+import jakarta.xml.ws.Endpoint;
+import ws.BanqueService;
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        String url="http://0.0.0.0:9090/";
+        Endpoint.publish(url,new BanqueService());
+        System.out.println("Web Service déployé sur :"+url);
+    }
+}
+
+
+
